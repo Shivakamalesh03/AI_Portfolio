c23dec61dc5f4850004e5e73df0e41e46aa3342a
Shivakamalesh45 <shivakamalesh45@gmail.com>
2025-04-04 22:05:59 +0530

    m

--- src/Components/TopNavbar.tsx
+++ src/Components/TopNavbar.tsx
@@ -68,11 +68,15 @@ return (
           <button onClick={handleResumeClick}>
           <img src={ResumeIcon} alt="Resume" className="w-6 h-6" />
           </button>
-          <a href="/contact">
+          <Link 
+                to="/contact"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
   <button>
     <img src={EmailIcon} alt="Email" className="w-6 h-6" />
   </button>
-</a>
+  </Link>
+
 
 
 
