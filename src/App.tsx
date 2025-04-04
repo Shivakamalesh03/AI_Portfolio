18bc1ca17bd2348a9cb430540e435cda9cc959d1
Shivakamalesh03 <shivakamalesh45@gmail.com>
2025-04-04 13:40:24 +0530

    fix : simple name change of component for consistency REnderComp to RenderPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import "./index.css";
 import TopNavbar from "./Components/TopNavbar";
-import RenderPage from "./Components/RenderComp";
+import RenderPage from "./Components/RenderPage";
 import Footnotes from "./Components/Footnotes";
 
 function App() {
