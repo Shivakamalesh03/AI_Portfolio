77096b58d7e3b58f198b8bbf58072740f8a5ea66
Shivakamalesh45 <shivakamalesh45@gmail.com>
2025-04-04 15:53:48 +0530

    yesu

--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -21,7 +21,7 @@ const LandingPage = () => {
               Shivakamalesh
             </h1>
             <p className="text-xl italic text-gray-700 dark:text-gray-400 mt-2">
-              "Free your Mind"
+              
               Passionate about web development, data analysis, and creating meaningful digital experiences. I enjoy turning ideas into action through clean code, clear communication, and collaborative problem-solving.
 
 
